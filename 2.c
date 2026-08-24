276f96c06a3bfecc09f8b4bd089dd49e748c5502
Gabriel Souza <gabriel.00000865594@unicap.br>
2026-08-23 22:59:09 -0300

    Questão 2

---
+++ 2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+    float nota1;
+    float nota2;
+    float nota3;
+    float media;
+
+    printf("Nota 1: ");
+    scanf("%f", &nota1);
+
+    printf("Nota 2: ");
+    scanf("%f", &nota2);
+
+    printf("Nota 3: ");
+    scanf("%f", &nota3);
+
+    media = (nota1 + nota2 + nota3) / 3;
+
+    printf("Media: %.2f\n", media);
+
+    return 0;
+}
